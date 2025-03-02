c6b097907905806083b83c2ef703c3b008a21834
efedincer <dincer.efe@gmail.com>
2025-03-02 19:57:05 +0300

    all layout things done and notification component added

---
+++ VolerAdminDashboard/ViewComponents/Notifications.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VolerAdminDashboard.ViewComponents;
+
+public class Notifications : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
