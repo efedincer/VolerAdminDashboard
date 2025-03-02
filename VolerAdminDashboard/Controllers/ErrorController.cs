6208125e497d1a1b3a2b83ded6596a2733e860be
efedincer <dincer.efe@gmail.com>
2025-03-02 19:33:24 +0300

    error controller and its own actions and also views of them added and configured in program.cs

---
+++ VolerAdminDashboard/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VolerAdminDashboard.Controllers;
+
+public class ErrorController : Controller
+{
+    [Route("Error/403")]
+    public IActionResult Error403() => View();
+
+    [Route("Error/404")]
+    public IActionResult Error404() => View();
+
+    [Route("Error/500")]
+    public IActionResult Error500() => View();
+
+}
--- VolerAdminDashboard/Program.cs
+++ VolerAdminDashboard/Program.cs
@@ -13,10 +13,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseHttpsRedirection();
 app.UseRouting();
