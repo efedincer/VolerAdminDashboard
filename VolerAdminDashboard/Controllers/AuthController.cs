69c02884fe13a2c9a7107cba36b65ebb9956f096
efedincer <dincer.efe@gmail.com>
2025-03-02 19:10:31 +0300

    auth pages done

---
+++ VolerAdminDashboard/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VolerAdminDashboard.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Login() => View();
+    public IActionResult Register() => View();
+    public IActionResult ForgotPassword() => View();
+}
---
+++ VolerAdminDashboard/Controllers/ErrorController1cs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VolerAdminDashboard.Controllers;
+
+public class ErrorController1cs : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
